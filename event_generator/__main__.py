75dfbdc38260a2332a68dc0efda151dbfe05564a
Chumakov Mikhail <mikhail.ch2011@yandex.ru>
2022-06-05 18:57:31 +0300

    Косметические изменения

--- event_generator/__main__.py
+++ event_generator/__main__.py
@@ -16,6 +16,7 @@ from event_generator.interface import generate_events
     help="Count of films for events generator or ALL",
 )
 def cli_generator_events(users: int, events: int, films: Optional[int]):
+    assert events <= 500
     generate_events(number_users=users, number_events=events, numbers_films=films)
 
 

bac9f1ef6be7a1b9e3b61acd3b86653eba464e48
Chumakov Mikhail <mikhail.ch2011@yandex.ru>
2022-06-10 01:37:03 +0300

    Bad status validation was added

--- rs_api/src/api/v1/rs_movies.py
+++ rs_api/src/api/v1/rs_movies.py
@@ -5,7 +5,7 @@ from uuid import UUID
 
 import backoff
 from db.elastic import ElasticSearchEngine, get_es_search
-from fastapi import APIRouter, Depends, HTTPException, Query
+from fastapi import APIRouter, HTTPException
 from models.rs_models import (BookmarkEvent, Movie, MovieBrief, RatingEvent, ViewEvent,
                               WatchEvent)
 from urllib3 import PoolManager
@@ -19,7 +19,7 @@ def get_movie(movie_uuid: str):
     try:
         http = PoolManager()
         movie = http.request("GET", f"http://10.5.0.1/api/v1/films/{movie_uuid}")
-        print(movie)
+
         if movie.status == 200:
             pp(loads(movie.data.decode("UTF-8")))
             movie = Movie(**loads(movie.data.decode("UTF-8")))
@@ -27,27 +27,30 @@ def get_movie(movie_uuid: str):
             return movie
 
     except HTTPError:
-        logger.error(HTTPError)
+        return
 
 
 @router.get(
     path="/{person_uuid}",
     name="Рекомендация для пользователя.",
     description="Получение детальной информации рекомендованых фильмов.",
-    response_model=list[Movie],
+    response_model=list[Movie]
 )
 async def genre_details(
     person_uuid: UUID,
-    # genre_service: GenreService = Depends(get_genre_service),
 ):
     es_ser = ElasticSearchEngine(get_es_search())
 
     films = await es_ser.get_by_pk(table="movies", pk=person_uuid)
 
-    films = films.get("movies")
     if films is None:
-        return HTTPStatus.NOT_FOUND
+        raise HTTPException(status_code=404, detail="Пользователь не найден.")
+
     movies = []
     for film_id in films:
-        movies.append(get_movie(film_id))
+        film_info = get_movie(film_id)
+        if film_info is None:
+            raise HTTPException(status_code=404, detail="фильм не найден.")
+
+        movies.append(film_info)
     return movies

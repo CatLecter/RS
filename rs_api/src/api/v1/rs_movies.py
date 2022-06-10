384632b9a5b70bfb63e0030c879836db610b4f3f
Chumakov Mikhail <mikhail.ch2011@yandex.ru>
2022-06-11 00:47:15 +0300

    Исправления 6 бага
    Давайте поправим эту функцию так, чтобы она могла забирать в list-апишке по списку id, сейчас у вас здесь максимально неэффективно. Для 10 рекомендаций сделаете 10 запросов. И тут же второй момент, возьмите aiohttp или httpx для того, чтобы делать запросы. Не стоит делать синхронные реалиазации в асинхронном приложении

--- rs_api/src/api/v1/rs_movies.py
+++ rs_api/src/api/v1/rs_movies.py
@@ -1,7 +1,12 @@
+import asyncio
+import nest_asyncio
 from json import loads
 from uuid import UUID
+from typing import Literal
 
+from pprint import pp
 import backoff
+from aiohttp import ClientSession, TCPConnector
 from http import HTTPStatus
 from db.elastic import ElasticSearchEngine, get_es_search
 from fastapi import APIRouter, HTTPException
@@ -10,6 +15,40 @@ from urllib3 import PoolManager
 from urllib3.exceptions import HTTPError
 
 router = APIRouter(prefix="/rs_movies", tags=["Рекомендованые Фильмы"])
+nest_asyncio.apply()
+
+
+async def get_movies(movie_uuid: UUID) -> Movie | None:
+    url = f"http://10.5.0.1/api/v1/films/{movie_uuid}"
+    print(url)
+    async with ClientSession(connector=TCPConnector(verify_ssl=False)) as session:
+        async with session.get(url) as response:
+            if response.status == HTTPStatus.OK:
+                return Movie(**await response.json())
+
+
+async def get_rec_movies(user_uuid: UUID) -> dict[UUID, list[UUID]]:
+    return {
+        user_uuid: await (ElasticSearchEngine(get_es_search())
+                          .get_by_pk(table="movies", pk=user_uuid))
+    }
+
+
+async def get_request(uuid: list[UUID], type_: Literal["movies", "rs_films"]):
+    if type_ == "movies":
+        tasks = (get_movies(current_uuid) for current_uuid in uuid)
+    else:
+        tasks = (get_rec_movies(current_uuid) for current_uuid in uuid)
+
+    return await asyncio.gather(*tasks)
+
+
+def make_requests_by_uuid(
+    uuid: list[UUID],
+    type_: Literal["movies", "rs_films"]
+) -> list:
+    loop = asyncio.new_event_loop()
+    return loop.run_until_complete(get_request(uuid=uuid, type_=type_))
 
 
 @backoff.on_exception(backoff.expo, HTTPError, max_tries=3)
@@ -32,7 +71,7 @@ def get_movie(movie_uuid: str):
     description="Получение детальной информации рекомендованых фильмов.",
     response_model=list[Movie]
 )
-async def genre_details(
+async def rs_movie4user(
     person_uuid: UUID,
 ):
     es_ser = ElasticSearchEngine(get_es_search())
@@ -52,3 +91,32 @@ async def genre_details(
 
         movies.append(film_info)
     return movies
+
+
+@router.post(
+    path="/recommendation_movies",
+    name="Рекомендации для пользователей.",
+    description="Получение детальной информации рекомендованых фильмов пользователей.",
+    # response_model=dict[str, list[Movie]]
+)
+def rs_movie4users(
+    persons_uuid: list[UUID]
+):
+    persons_with_films = make_requests_by_uuid(uuid=persons_uuid, type_="rs_films")
+
+    persons_and_films = {}
+    for el in persons_with_films:
+        persons_and_films = persons_and_films | el
+
+    persons_and_films = {
+        user_uuid:
+            persons_and_films[user_uuid]['movies'] for user_uuid in persons_and_films
+    }
+
+    movies_id = []
+    for user_id in persons_and_films:
+        movies_id += persons_and_films[user_id]
+
+    movies_full_info = make_requests_by_uuid(uuid=movies_id, type_="movies")
+
+    return movies_full_info
